d6419177a8683dfb0b36c7d7af9882ea50dbc233
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2022-01-18 00:10:41 +0200

    Update

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SUHttpServer/Routing/RoutingTable.cs
@@ -0,0 +1,68 @@
+﻿using SUHttpServer.Common;
+using SUHttpServer.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUHttpServer.Routing
+{
+    public class RoutingTable : IRoutingTable
+    {
+        private readonly Dictionary<Method, Dictionary<string, Response>> routes;
+
+        public RoutingTable()
+            => routes = new Dictionary<Method, Dictionary<string, Response>>()
+            {
+                [Method.Get] = new(),
+                [Method.Post] = new(),
+                [Method.Put] = new(),
+                [Method.Delete] = new()
+            };
+
+
+        public IRoutingTable Map(string url, Method method, Response response)
+            => method switch
+            {
+                Method.Get => MapGet(url, response),
+                Method.Post => MapPost(url, response),
+                _ => throw new InvalidOperationException(
+                    $"Method '{method}' is not supported.")
+            };
+
+        public IRoutingTable MapGet(string url, Response response)
+        {
+            Guard.AgainstNull(url, nameof(url));
+            Guard.AgainstNull(response, nameof(response));
+
+            routes[Method.Get][url] = response;
+
+            return this;
+        }
+
+        public IRoutingTable MapPost(string url, Response response)
+        {
+            Guard.AgainstNull(url, nameof(url));
+            Guard.AgainstNull(response, nameof(response));
+
+            routes[Method.Post][url] = response;
+
+            return this;
+        }
+
+        public Response MatchRequest(Request request)
+        {
+            var requestMethod = request.Method;
+            var requestUrl = request.Url;
+
+            if(routes.ContainsKey(requestMethod)
+                || !routes[requestMethod].ContainsKey(requestUrl))
+            {
+                return new NotFoundResponse();
+            }
+
+            return routes[requestMethod][requestUrl];
+        }
+    }
+}
