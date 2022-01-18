7ca8252212ccdb6127eb08a4ab5c74e8a7e24862
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2022-01-18 22:19:04 +0200

    Update

--- C# Web/C# Web Basics/Web Server - HTTP Protocol/SUHttpServer/HTTP/ContentResponse.cs
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SUHttpServer/HTTP/ContentResponse.cs
@@ -1,9 +1,5 @@
 ﻿using SUHttpServer.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SUHttpServer.HTTP
 {
--- C# Web/C# Web Basics/Web Server - HTTP Protocol/SUHttpServer/Responses/BadRequestResponse.cs
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SUHttpServer/Responses/BadRequestResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SUHttpServer.HTTP
+﻿namespace SUHttpServer.HTTP
 {
     public class BadRequestResponse : Response
     {
