a6e809e6dbaef918a9de340f6454c4edb8cf9d5f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2022-09-25 22:27:29 +0300

    DoublyLinkedList Solved

--- Data Structures Fundamentals C#/Linear-Data-Structures Exercises/02.DoublyLinkedList/DoublyLinkedList.cs
+++ Data Structures Fundamentals C#/Linear-Data-Structures Exercises/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -7,47 +7,105 @@
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
         private Node<T> head;
+        private Node<T> tail;
 
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>(item, null, null);
+
+            if(Count == 0)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                var oldHead = head;
+                head = newNode;
+                head.Next = oldHead;
+                head.Next.Previous = head;
+            }
+
+            Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>(item, null, null);
+
+            if (Count == 0)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                var oldNode = tail;
+                tail.Next = newNode;
+                tail = tail.Next;
+                tail.Previous = oldNode;
+            }
+
+            Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            EnsureIsNotEmpty();
+            return head.Item;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            EnsureIsNotEmpty();
+            return tail.Item;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            EnsureIsNotEmpty();
+
+            var oldNode = head;
+            head = head.Next;
+            Count--;
+
+            return oldNode.Item;
         }
+
+
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            EnsureIsNotEmpty();
+
+            var oldTail = tail;
+            tail = tail.Previous;
+            Count--;
+            return oldTail.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = head;
+
+            while (node != null)
+            {
+                yield return node.Item;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void EnsureIsNotEmpty()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
         }
     }
 }
--- Data Structures Fundamentals C#/Linear-Data-Structures Exercises/02.DoublyLinkedList/Node.cs
+++ Data Structures Fundamentals C#/Linear-Data-Structures Exercises/02.DoublyLinkedList/Node.cs
@@ -2,8 +2,22 @@
 {
     public class Node<T>
     {
+        public Node()
+        {
+
+        }
+
+        public Node(T item, Node<T> next, Node<T> previous)
+        {
+            Item = item;
+            Next = next;
+            Previous = previous;
+        }
+
         public T Item { get; set; }
 
         public Node<T> Next { get; set; }
+
+        public Node<T> Previous { get; set; }
     }
 }
