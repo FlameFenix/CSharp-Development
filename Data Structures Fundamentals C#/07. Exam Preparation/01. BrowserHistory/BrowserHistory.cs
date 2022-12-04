de2c65068be634047ba89d7d65bb455d36b2c19c
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2022-12-04 19:28:31 +0200

    Update :100:

--- Data Structures Fundamentals C#/07. Exam Preparation/01. BrowserHistory/BrowserHistory.cs
+++ Data Structures Fundamentals C#/07. Exam Preparation/01. BrowserHistory/BrowserHistory.cs
@@ -2,219 +2,129 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
     using _01._BrowserHistory.Interfaces;
 
     public class BrowserHistory : IHistory
     {
-        private ILink[] links;
-        private int arrayCapacity = 4;
-
-        public BrowserHistory()
-        {
-            links = new ILink[arrayCapacity];
-        }
-
-        public ILink this[int index]
-        {
-            get
-            {
-                // 5 4 3 2 1
-                ValidateIndex(index);
-                return links[Size - index];
-            }
-            set
-            {
-                // 1 2 3 4 5
-                ValidateIndex(index);
-                links[index] = value;
-            }
-        }
-
-        private void ValidateIndex(int index)
-        {
-            if(index < 0 && index > Size)
-            {
-                throw new IndexOutOfRangeException();
-            }
-        }
-
+        private LinkedList<ILink> history = new LinkedList<ILink>();
         public int Size { get; private set; }
 
         public void Clear()
         {
-            links = default;
+            history.Clear();
             Size = 0;
         }
 
         public bool Contains(ILink link)
         {
-            foreach (var linkItem in links)
-            {
-                if (linkItem.Equals(link))
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return history.Contains(link);
         }
 
         public ILink DeleteFirst()
         {
-            CheckCollection();
+            var link = history.LastOrDefault();
 
-            var oldLink = links[0];
-
-            for (int i = 0; i < Size - 1; i++)
-            {
-                links[i] = links[i + 1];
-            }
-
-            links[Size - 1] = default;
-
-            Size--;
-
-            return oldLink;
-        }
-
-        public ILink DeleteLast()
-        {
-            CheckCollection();
-
-            var oldLink = links[Size - 1];
-            links[Size - 1] = default;
-            Size--;
-
-            return oldLink;
-        }
-
-        public ILink GetByUrl(string url)
-        {
-            foreach (var link in links)
-            {
-                if (link.Url.Equals(url))
-                {
-                    return link;
-                }
-            }
-
-            return null;
-        }
-
-        public ILink LastVisited()
-        {
-            CheckCollection();
-
-            return links[Size - 1];
-        }
-
-        public void Open(ILink link)
-        {
-            if(links.Length == Size)
-            {
-                arrayGrow();
-            }
-
-            links[Size++] = link;
-        }
-
-        private void arrayGrow()
-        {
-            var array = new ILink[links.Length * 2];
-            Array.Copy(links, array, Size);
-            links = array;
-        }
-
-        public int RemoveLinks(string url)
-        {
-            int counter = 0;
-
-            var index = indexOf(url);
-
-            while (index != -1)
-            {
-                for (int i = index; i < Size - 1; i++)
-                {
-                    links[i] = links[i + 1];
-                }
-
-                links[Size - 1] = default;
-                Size--;
-                counter++;
-
-                index = indexOf(url);
-            }
-
-            if(counter == 0)
+            if (link == null)
             {
                 throw new InvalidOperationException();
             }
 
-            return counter;
+            history.RemoveFirst();
+
+            Size--;
+
+            return link;
+        }
+
+        public ILink DeleteLast()
+        {
+            var link = history.FirstOrDefault();
+
+            if (link == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            history.RemoveLast();
+
+            Size--;
+
+            return link;
+        }
+
+        public ILink GetByUrl(string url)
+        {
+            return history.Where(x => x.Url.ToLower().Equals(url.ToLower())).FirstOrDefault();
+        }
+
+        public ILink LastVisited()
+        {
+            if (Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return history.FirstOrDefault();
+        }
+
+        public void Open(ILink link)
+        {
+            history.AddFirst(link);
+            Size++;
+        }
+
+        public int RemoveLinks(string url)
+        {
+
+            
+            var deletedLinks = history.Where(x => x.Url.Contains(url)).ToArray();
+
+            if (deletedLinks.Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldCount = Size;
+
+            foreach (var link in deletedLinks)
+            {
+                history.Remove(link);
+                Size--;
+            }
+
+            return oldCount - Size;
         }
 
         public ILink[] ToArray()
         {
-            var array = new ILink[Size];
-
-            for (int i = 0; i < Size; i++)
-            {
-                array[i] = links[Size - 1 - i];
-            }
-
-            return array;
+            return history.ToArray();
         }
 
         public List<ILink> ToList()
         {
-            var list = new List<ILink>(links.Length);
-
-            for (int i = Size - 1; i >= 0; i--)
-            {
-                list.Add(links[i]);
-            }
-
-            return list;
+            return history.ToList();
         }
 
         public string ViewHistory()
         {
             var sb = new StringBuilder();
 
-            if(Size == 0)
+            var currentHistory = ToList();
+
+            if (currentHistory.Count == 0)
             {
                 sb.AppendLine("Browser history is empty!");
-                return sb.ToString();
             }
 
-            for (int i = Size - 1; i >= 0; i--)
+            foreach (var link in currentHistory)
             {
-                sb.AppendLine($"{links[i]}");
+                sb.AppendLine(link.ToString());
             }
 
+            
             return sb.ToString();
-        }
-
-        private void CheckCollection()
-        {
-            if(Size == 0)
-            {
-                throw new InvalidOperationException();
-            }
-        }
-
-        private int indexOf(string url)
-        {
-            for (int i = 0; i < Size; i++)
-            {
-                if (links[i].Url.ToLower().Contains(url.ToLower()))
-                {
-                    return i;
-                }
-            }
-
-            return -1;
         }
     }
 }
